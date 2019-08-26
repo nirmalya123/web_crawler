a69204e69812ef6606a12e8877d12af58432b1fa
nirmad <nirmalya.do@outlook.com>
2019-08-27 00:29:07 +0530

    Addrd threading.

--- continuecrawl.py
+++ continuecrawl.py
@@ -3,7 +3,8 @@ import sys
 import time
 # from flask import Flask
 import urllib
-
+import threading
+import time
 # from urllib import urllib.parse
 # import urllib.parse
 
@@ -22,6 +23,9 @@ from collections import Counter
 
 main_dict = {}  # URL - Count
 target_count_list = []
+
+main_dict_lock = threading.Lock()
+target_count_list_lock = threading.Lock()
 
 wikiname = "wikipedia_en_all_nopic"
 if True:
@@ -76,7 +80,7 @@ def find_first_link(url):
                 article_link = element.find("a", recursive=False).get('href')
                 break
     except Exception as e:
-        logger.error("Exception while parsing html. {}".format(e))
+        logger.debug("Exception while parsing html. {}".format(e))
         return (None, None)
 
     if not article_link:
@@ -90,55 +94,82 @@ def find_first_link(url):
 
 def continue_crawl(search_history, target_url, max_steps=25):
     if search_history[-1] in target_url:
-        logger.info("We've found the target article!")
-        target_count_list.append(target_url[-1])
+        logger.debug("We've found the target article!")
+        with target_count_list_lock:
+            target_count_list.append(target_url[-1])
         return False
     elif len(search_history) > max_steps:
-        logger.warning("The search has gone on suspiciously long, aborting search!")
+        logger.debug("The search has gone on suspiciously long, aborting search!")
         return False
     elif search_history[-1] in search_history[:-1]:
-        logger.warning("We've arrived at an article we've already seen, aborting search!")
+        logger.debug("We've arrived at an article we've already seen, aborting search!")
         return False
     else:
         return True
 
-# article_chain = [start_url]
 
-
-def run_crawler():
-    count = 0
-    random_resp_url = None
-    while continue_crawl(article_chain, target_url):
-        logger.info("{} - Article to crawl - {}".format(count, article_chain[-1]))
-        first_link, resp_url = find_first_link(article_chain[-1])
-        if not first_link:
-            logger.warning("We've arrived at an article with no links, aborting search!")
-            break
-        if count == 0:
-            random_resp_url = resp_url
-        article_chain.append(first_link)
-        count = count + 1
-        # time.sleep(3)  # Slow things down so as to not hammer Wikipedia's servers
-    logger.info("-------------------------------------")
-    main_dict[random_resp_url] = count
-    logger.info("| {} --- {}".format(random_resp_url, count))
-    logger.info("-------------------------------------")
+def run_crawler(loop_count):
+    article_chain = [start_url]
+    for i in range(loop_count):
+        count = 0
+        random_resp_url = None
+        while continue_crawl(article_chain, target_url):
+            logger.debug("{} - Article to crawl - {}".format(count, article_chain[-1]))
+            first_link, resp_url = find_first_link(article_chain[-1])
+            if not first_link:
+                logger.debug("We've arrived at an article with no links, aborting search!")
+                break
+            if count == 0:
+                random_resp_url = resp_url
+            article_chain.append(first_link)
+            count = count + 1
+            # time.sleep(3)  # Slow things down so as to not hammer Wikipedia's servers
+        logger.debug("-------------------------------------")
+        with main_dict_lock:
+            main_dict[random_resp_url] = count
+        logger.debug("| {} --- {}".format(random_resp_url, count))
+        logger.debug("-------------------------------------")
 
 if __name__ == "__main__":
-    for i in range(100):
-        logger.info(">>>>>>>>>>>>>>>>>")
-        logger.info(">>>>>>>>>>>>>>>>> {} <<<<<<<<<<<<<<<<<<<<".format(i))
-        logger.info(">>>>>>>>>>>>>>>>>")
-        article_chain = [start_url]
-        run_crawler()
+    time_data_dict = {}
+    main_count = 10
+    l = [1]
+    l.extend(range(2, main_count+2, 2))
+    for j in l:
+        start_time = time.time()
+        total_iteration = 10
+        block_count = j
+        main_loop = total_iteration // block_count
+        threads = list()
+        for i in range(main_loop):
+            logger.debug(">>>>>>>>>>>>>>>>>")
+            logger.debug(">>>>>>>>>>>>>>>>> {} <<<<<<<<<<<<<<<<<<<<".format(i))
+            logger.debug(">>>>>>>>>>>>>>>>>")
+            x = threading.Thread(target=run_crawler, args=(block_count,))
+            threads.append(x)
+            x.start()
+            # article_chain = [start_url]
 
-    logger.info("-------------------------------------")
-    pprint(main_dict)
-    logger.info("-------------------------------------")
-    logger.info("-------------------------------------")
-    # pprint(target_count_list)
-    c = Counter(target_count_list)
-    pprint(c)
-    logger.info("-------------------------------------")
+            # run_crawler(block_count)
+        for index, thread in enumerate(threads):
+            thread.join()
+        time_data_dict[j] = time.time() - start_time
+        logger.info("#Threads {} --- #URL {} --- Time {}".format(main_loop, block_count, time_data_dict[j]))
+        # logger.info("-------------------------------------")
+        # with main_dict_lock:
+        #     pprint(main_dict)
+        # logger.info("-------------------------------------")
+        # logger.info("-------------------------------------")
+        # pprint(target_count_list)
+        # with target_count_list_lock:
+        #     c = Counter(target_count_list)
+        # pprint(c)
+        # logger.info("-------------------------------------")
+        # pprint(time_data_dict)
+        # time.sleep(5)
 
-
+    logger.info("====================================================")
+    logger.info("====================================================")
+    pprint(time_data_dict)
+    logger.info("====================================================")
+    logger.info("====================================================")
---
+++ documentation/Observation.md
@@ -0,0 +1,14 @@
+## Observation 1
+As the number of the thread increases and number of URL-iteration decreases time increases.
+Data as below-
+
+| Thread Count | Number of times fetch random URL | Time taken (in seconds) |
+|----|-----|-----|
+| 10 | 1 | 18.424436330795288 |
+| 5 | 2 | 13.308493614196777 |
+| 2 | 4 | 4.888773679733276 |
+| 1 | 6 | 1.8558893203735352 |
+| 1 | 8 | 2.7798664569854736 |
+| 1 | 10 | 5.000336170196533 |
+
+----------------------------
