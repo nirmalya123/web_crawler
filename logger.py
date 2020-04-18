faac0fd62335fe341eb3853722e23654aa5766f6
nirmad <nirmalya.do@outlook.com>
2020-04-18 18:58:34 +0530

    logger updated.

--- logger.py
+++ logger.py
@@ -8,13 +8,13 @@ logger = logging.getLogger(__name__)
 # Create handlers
 c_handler = logging.StreamHandler()
 f_handler = logging.FileHandler('crawler.log')
-c_handler.setLevel(logging.DEBUG)
+c_handler.setLevel(logging.INFO)
 f_handler.setLevel(logging.DEBUG)
 
 # Create formatters and add it to handlers
 # c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-c_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+c_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - [%(relativeCreated)6d %(threadName)s] - %(message)s')
+f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - [%(relativeCreated)6d %(threadName)s] - %(message)s')
 c_handler.setFormatter(c_format)
 f_handler.setFormatter(f_format)
 # Add handlers to the logger
